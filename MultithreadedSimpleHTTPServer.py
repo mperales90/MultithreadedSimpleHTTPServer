5d13e36745c1ef4482d8c5c200b3bd7eb5356996
mperales90 <mariann.perales@gmail.com>
2019-02-10 02:41:26 +0000

    cambios feos

--- MultithreadedSimpleHTTPServer.py
+++ MultithreadedSimpleHTTPServer.py
@@ -5,13 +5,38 @@ try:
     from SocketServer import ThreadingMixIn
     from SimpleHTTPServer import SimpleHTTPRequestHandler
     from BaseHTTPServer import HTTPServer
+    import thread
+    import threading
+    import SocketServer, time
+    import multiprocessing
+#    import dataQueueServer
 except ImportError:
     # Python 3.x
+    import thread
+    import threading
     from socketserver import ThreadingMixIn
     from http.server import SimpleHTTPRequestHandler, HTTPServer
+    import SocketServer, time
+    import multiprocessing 
+#    import dataQueueServer
 
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        cur_thread = threading.current_thread()
+        response = "{}: {}".format(cur_thread.name, data)
+#        videoPool.add(data)
+        print "Output! %s" % data
+        self.request.sendall(response)
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
 
 import sys
 import os
@@ -33,10 +58,24 @@ if sys.argv[2:]:
 
 print('Started HTTP server on ' +  interface + ':' + str(port))
 
-server = ThreadingSimpleServer((interface, port), SimpleHTTPRequestHandler)
-try:
-    while 1:
-        sys.stdout.flush()
-        server.handle_request()
-except KeyboardInterrupt:
-    print('Finished.')
+def start_server(interface, port, ThreadedTCPRequestHandler):
+
+
+
+#    videoPool = QueueServer()
+    print (interface)
+    print (port)
+#    server = ThreadingSimpleServer((interface, port), SimpleHTTPRequestHandler)
+    server = ThreadedTCPServer((interface, port), ThreadedTCPRequestHandler)
+    server.serve_forever()
+
+thread.start_new_thread(start_server (interface, port, ThreadedTCPRequestHandler),())
+
+#try:
+#    while 1:
+#        sys.stdout.flush()
+#        server.handle_request()
+#except KeyboardInterrupt:
+#    print('Finished.')
+
+print('The server is running but my script is still executing!')
